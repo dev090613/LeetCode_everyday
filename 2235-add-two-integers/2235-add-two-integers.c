1cedc36ea2907b4ec19a876b7e8ee90342b47354
kokoro <128305501+dev090613@users.noreply.github.com>
2023-03-24 00:30:00 +0900

    Time: 4 ms (35.35%), Space: 5.7 MB (23.65%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.c
@@ -0,0 +1,14 @@
+/*pseudo code
+begin
+    print "Enter 2 no"
+    read num1, num2
+    sum = num1 + num2
+    print "sum is" sum
+end
+*/
+
+int sum(int num1, int num2){
+    int sum;
+    sum = num1 + num2;
+    return sum;
+}
