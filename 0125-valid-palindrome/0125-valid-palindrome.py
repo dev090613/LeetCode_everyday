e4a06622598dd051f63a52e9a23f944137836388
kokoro <128305501+dev090613@users.noreply.github.com>
2023-04-21 10:03:11 +0900

    Time: 218 ms (5.09%), Space: 19.5 MB (5.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,3 @@
-# string.isalnum()
-# The isalnum() method returns True if all characters in the string are alphanumeric (either alphabets or numbers). If not, it returns False.
-
-# re.sub('찾을 패턴', '변경할 내용', '원본')
-# [^a-z]: 선두에 위치할 경우 해당 문자 패턴(알파벳)이 아닌 것 매칭
-
 # 방법1
 # class Solution:
 #     def isPalindrome(self, s: str) -> bool:
@@ -17,10 +11,30 @@
 #         return True
 
 
-# 방법2: isalnum() 이 아닌 re.sub 사용
+# # 방법2: isalnum() 이 아닌 re.sub 사용
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = s.lower()
+#         s = re.sub('[^a-z0-9]', '', s) # important\U0001f339
+        
+#         return s == s[::-1]
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s_lower = s.lowwer()
+#         left, right = s_lower[0], s_lower[-1]
+#         while left<len(s_lower)/2:
+#             if left != right:
+#                 return False
+#             left += 1
+#             right += 1
+#         return True
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        s = re.sub('[^a-z0-9]', '', s) # important\U0001f339
-        
-        return s == s[::-1]
+        s_list = [char.lower() for char in s if char.isalnum()]
+        while len(s_list) > 1:
+            if s_list.pop(0) != s_list.pop():
+                return False
+        return True
+
