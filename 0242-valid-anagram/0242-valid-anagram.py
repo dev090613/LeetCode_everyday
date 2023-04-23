1ba65b803b957e4d60d43e74d9fbce182f17746a
kokoro <128305501+dev090613@users.noreply.github.com>
2023-04-23 10:12:44 +0900

    Time: 30 ms (99.25%), Space: 14.4 MB (58.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,24 @@
-# 방법1: sort
+
 # 방법2: index 26
 # counter도 사용할 수 있지 않을까..?
-# 방법1: sorted()
+# # 방법1: sorted()/ 64%, 10%
+# comment: sorting is last resort when coding an algorithm.
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return sorted(s) == sorted(t)
+
+#방법2: index26, string.count()
+# count() method returns an integer that denotes number of times a substring occurs in a given string. 
+# https://www.geeksforgeeks.org/python-string-count-method/
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
-        
+        if len(s) != len(t):
+            return False
+        for char in "abcdefghijklmnopqrstuvwxyz":
+            if s.count(char) != t.count(char):
+                return False
+        return True
+
         
 # Follow up
 
