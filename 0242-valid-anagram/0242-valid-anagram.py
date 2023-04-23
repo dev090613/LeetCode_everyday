5fbc0cfd416468db9c1290fce469ea5f0083b501
kokoro <128305501+dev090613@users.noreply.github.com>
2023-04-23 10:38:01 +0900

    Time: 28 ms (99.53%), Space: 14.4 MB (94.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -12,14 +12,14 @@
 # # count() method returns an integer that denotes number of times a substring occurs in a given string. 
 # # https://www.geeksforgeeks.org/python-string-count-method/
 # # It's practically O(1), however, we're using the built in str.count() method, which theoretically is O(n), and since we call it 26 times this solution is O(26*(n+m)). But, python built-in library methods are efficiently coded for speed ups and can generally be used for efficient solutions.
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         if len(s) != len(t):
-#             return False
-#         for char in "abcdefghijklmnopqrstuvwxyz":
-#             if s.count(char) != t.count(char):
-#                 return False
-#         return True
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        for char in "abcdefghijklmnopqrstuvwxyz":
+            if s.count(char) != t.count(char):
+                return False
+        return True
 
 # # 방법3: set(), count()/ 93% 32%
 # class Solution:
@@ -31,26 +31,26 @@
 #                 return False
 #         return True
         
-# 방법4: hash-map, dict.items()/ 51% 58%/ 비추천
-class Solution:
-    def isAnagram(self, s: str, t:str) -> bool:
-        if len(s) != len(t):
-            return False
-        hash_dict = dict() # it means, {}
-        for char in s:
-            if char in hash_dict:
-                hash_dict[char] += 1
-            else:
-                hash_dict[char] = 1
-        for char in t:
-            if char in hash_dict:
-                hash_dict[char] -= 1
-            else:
-                return False
-        for k,v in hash_dict.items():
-            if v != 0: # is not None과는 다르다. 이유는?
-                return False
-        return True
+# # 방법4: hash-map, dict.items()/ 60% 58%/ 비추천
+# class Solution:
+#     def isAnagram(self, s: str, t:str) -> bool:
+#         if len(s) != len(t):
+#             return False
+#         hash_dict = dict() # it means, {}
+#         for char in s:
+#             if char in hash_dict:
+#                 hash_dict[char] += 1
+#             else:
+#                 hash_dict[char] = 1
+#         for char in t:
+#             if char in hash_dict:
+#                 hash_dict[char] -= 1
+#             else:
+#                 return False
+#         for k,v in hash_dict.items():
+#             if v != 0: # is not None과는 다르다. 이유는?
+#                 return False
+#         return True
                 
 
 # # 방법5: collections.defaultdict()
