6d6cfab8b596d42e55c334c3af7ecf5571b9e3b8
kokoro <128305501+dev090613@users.noreply.github.com>
2023-04-23 10:21:18 +0900

    Time: 39 ms (92.53%), Space: 14.5 MB (31.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,35 @@
 
 # 방법2: index 26
 # counter도 사용할 수 있지 않을까..?
+
 # # 방법1: sorted()/ 64%, 10%
 # comment: sorting is last resort when coding an algorithm.
 # class Solution:
 #     def isAnagram(self, s: str, t: str) -> bool:
 #         return sorted(s) == sorted(t)
 
-#방법2: index26, string.count()
-# count() method returns an integer that denotes number of times a substring occurs in a given string. 
-# https://www.geeksforgeeks.org/python-string-count-method/
+# #방법2: index26, string.count()/ 99%, 58%
+# # count() method returns an integer that denotes number of times a substring occurs in a given string. 
+# # https://www.geeksforgeeks.org/python-string-count-method/
+# # It's practically O(1), however, we're using the built in str.count() method, which theoretically is O(n), and since we call it 26 times this solution is O(26*(n+m)). But, python built-in library methods are efficiently coded for speed ups and can generally be used for efficient solutions.
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+#         for char in "abcdefghijklmnopqrstuvwxyz":
+#             if s.count(char) != t.count(char):
+#                 return False
+#         return True
+
+# 방법3: set(), count()
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        for char in "abcdefghijklmnopqrstuvwxyz":
+        for char in set(s):
             if s.count(char) != t.count(char):
                 return False
         return True
-
         
 # Follow up
 
