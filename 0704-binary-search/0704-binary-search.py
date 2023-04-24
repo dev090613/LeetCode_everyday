7dc8656f218e97c167069cab360b682142973776
kokoro <128305501+dev090613@users.noreply.github.com>
2023-04-24 11:41:04 +0900

    Time: 255 ms (10.01%), Space: 15.5 MB (56.10%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,8 +1,38 @@
+# #방법1: for i in range(len(arr)) 69%, 96%
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         # ascending order로 정렬된 arr(nums)
+#         # nums 중에서 target을 찾고, 그것의 index를 반환하라
+#         for i in range(len(nums)):
+#             if nums[i] == target:
+#                 return i
+#         return -1
+
+# 방법2: binary search
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # ascending order로 정렬된 arr(nums)
-        # nums 중에서 target을 찾고, 그것의 index를 반환하라
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+        # 전제: 오름차순 정렬된 arr nums
+        # left와 right는 arr의 양 끝 인덱스
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid+1
+            else:
+                right = mid-1
         return -1
+        
+#         while left<=right:
+#             mid = (left+right)//2
+#             if nums[mid]==target:
+#                 return mid
+#             elif nums[mid]>target:
+#                 right = mid-1
+#             else:
+#                 left = mid+1
+        
+#         return -1
