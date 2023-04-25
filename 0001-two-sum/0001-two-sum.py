2d978161ba93daeb2fe7245f6872d105e1e3154e
kokoro <128305501+dev090613@users.noreply.github.com>
2023-04-25 11:12:30 +0900

    Time: 60 ms (80.20%), Space: 15 MB (56.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,25 @@
-# 방법1: Brute Force, O(n^2): 이중 for문, O(1): 
-# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+# # 방법1: Brute Force(2중 loop)/ O(n^2), O(1) / 21% 100%
+# # Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         # loop: list의 size만큼
+#         for i in range(len(nums)):
+#             # loop2: i+1 부터 마지막 인덱스까지
+#             for j in range(i+1, len(nums)-1 + 1):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+            
+            
+# 방법2: Two-pass Hash Table/ O(N), O(N)/ 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # loop: list의 size만큼
+        hashmap = {}
+        # In the first iteration, List의 element's의 key와 value를 hash map에 저장
         for i in range(len(nums)):
-            # loop2: i+1 부터 마지막 인덱스까지
-            for j in range(i+1, len(nums)-1 + 1):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-            
-            
-# 방법2: Two-pass Hash Table
+            hashmap[nums[i]] = i
+        #  in the second iteration, complement의 존재를 찾습니다.
+        # nums[i] + complement = target
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
