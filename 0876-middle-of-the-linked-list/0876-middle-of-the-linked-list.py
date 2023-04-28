a1e010644b0dcd665cf851e4dccf9a4d1c3eae77
kokoro <128305501+dev090613@users.noreply.github.com>
2023-04-28 14:39:40 +0900

    Time: 43 ms (7.38%), Space: 16.3 MB (5.85%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -1,18 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         slow, fast = head, head
+#         while fast and fast.next: # fast가 1칸 이동한 경우는 어차피 필요가 없으니
+#             fast = fast.next.next
+#             slow = slow.next
+#         # while fast:
+#         #     fast = fast.next
+#         #     if fast:
+#         #         fast = fast.next
+#         #     else: break
+#         #     slow = slow.next
+#         return slow
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow, fast = head, head
-        while fast and fast.next: # fast가 1칸 이동한 경우는 어차피 필요가 없으니
-            fast = fast.next.next
+        while fast and fast.next:
             slow = slow.next
-        # while fast:
-        #     fast = fast.next
-        #     if fast:
-        #         fast = fast.next
-        #     else: break
-        #     slow = slow.next
+            fast = fast.next.next
         return slow
