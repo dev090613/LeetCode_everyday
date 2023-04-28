b21a943e5bcd5be0e9f18b1ea92bc900af4ab9c0
kokoro <128305501+dev090613@users.noreply.github.com>
2023-04-29 00:48:51 +0900

    Time: 41 ms (7.38%), Space: 16.3 MB (5.85%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -1,26 +1,12 @@
-# # Definition for singly-linked list.
-# # class ListNode:
-# #     def __init__(self, val=0, next=None):
-# #         self.val = val
-# #         self.next = next
-# class Solution:
-#     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         slow, fast = head, head
-#         while fast and fast.next: # fast가 1칸 이동한 경우는 어차피 필요가 없으니
-#             fast = fast.next.next
-#             slow = slow.next
-#         # while fast:
-#         #     fast = fast.next
-#         #     if fast:
-#         #         fast = fast.next
-#         #     else: break
-#         #     slow = slow.next
-#         return slow
-
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        return slow
+# Time Complexity :  O(n)
+# Space Complexity : O(1)
+class Solution(object):
+    def middleNode(self, head):
+        # We need two pointers, one is head with one step each iteration, and the other is tmp with two steps each iteration.
+        temp = head
+        while temp and temp.next:
+            # In each iteration, we move head one node forward and we move temp two nodes forward...
+            head = head.next
+            temp = temp.next.next
+        # When temp reaches the last node, head will be exactly at the middle point...
+        return head
