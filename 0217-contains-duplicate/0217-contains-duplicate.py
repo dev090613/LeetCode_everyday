b99de732807754ac1b708671b722493ac6610d95
kokoro <128305501+dev090613@users.noreply.github.com>
2023-05-09 19:24:02 +0900

    Time: 574 ms (27.11%), Space: 30.2 MB (30.39%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,6 +1,18 @@
+# 중복되는 값이 있는 경우 True
+# [1,2,3,1]
+# True
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # 방법1: set()과 list()의 크기 차이를 이용
-        if len(nums) != len(set(nums)):
-            return True
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
         return False
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         # # 방법1: set()과 list()의 크기 차이를 이용/ 61% 77%
+#         # if len(nums) != len(set(nums)):
+#         #     return True
+#         # return False
