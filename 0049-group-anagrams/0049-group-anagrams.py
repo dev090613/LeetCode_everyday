366335003e9afd9517e12a02a1a0b60ca41f947b
kokoro <128305501+dev090613@users.noreply.github.com>
2023-05-10 10:48:01 +0900

    Time: 106 ms (54.16%), Space: 20.5 MB (10.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,9 @@
-# The functionality of both dictionaries and defaultdict are almost same except for the fact that defaultdict never raises a KeyError. It provides a default value for the key that does not exists.
-
-# In python, list can't be key in dictionary. 
-# list can be converted to tuple. 
-# 따라서, list는 튜플로 변환 후 dict의 key로 사용할 수 있다.
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # KeyError 대신 empty list 반환
         res = defaultdict(list)
         
-        # 동일한 counter를 가진 str들끼리 묶을 것이다.
-        # { [counter1] : [str1, str2], 
-        #   [counter2] : [str3] }
         for str in strs:
-            counter = [0] * 26 # a-z의 반복횟수 저장될 것
-            for char in str:
-                counter[ord(char) - ord('a')] += 1
-            res[tuple(counter)].append(str) # counter 같은 str끼리 모일 것
-        return res.values()
+            
+            res[tuple(sorted(str))].append(str) 
+        return list(res.values())
         
