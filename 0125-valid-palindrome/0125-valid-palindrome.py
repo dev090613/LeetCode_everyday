86d985cc11c98b512fe3996544471cf2ec7dd1e3
kokoro <128305501+dev090613@users.noreply.github.com>
2023-05-12 23:40:44 +0900

    Time: 128 ms (5.19%), Space: 17.1 MB (19.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,40 +1,33 @@
-# 방법1
-# class Solution:
-#     def isPalindrome(self, s: str) -> bool:
-#         strs = [char.lower() for char in s if char.isalnum()]
-#         print(strs)
-        
-#         while len(strs) > 1:
-#             if strs.pop(0) != strs.pop():# check!
-#                 return False
-            
-#         return True
-
-
 # # 방법2: isalnum() 이 아닌 re.sub 사용
 # class Solution:
 #     def isPalindrome(self, s: str) -> bool:
 #         s = s.lower()
-#         s = re.sub('[^a-z0-9]', '', s) # important\U0001f339
+#         s = re.sub('[^a-z0-9]', '', s)
         
 #         return s == s[::-1]
+#----------------------------------------------------------
 
-# class Solution:
-#     def isPalindrome(self, s: str) -> bool:
-#         s_lower = s.lowwer()
-#         left, right = s_lower[0], s_lower[-1]
-#         while left<len(s_lower)/2:
-#             if left != right:
-#                 return False
-#             left += 1
-#             right += 1
-#         return True
-
+# 학습용 솔루션: Space O(1)로 풀어보기, isalnum 사용하지 않기 
+# 조건: after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters
+# input: "A man, a plan, a canal: Panama", output: True
+# idea: Two pointer
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s_list = [char.lower() for char in s if char.isalnum()]
-        while len(s_list) > 1:
-            if s_list.pop(0) != s_list.pop():
+        left, right = 0, len(s)-1
+        while left < right:
+            while (not self.alphaNum(s[left])) and (left < right):
+                left += 1
+                print("left is ", s[left])
+            while (not self.alphaNum(s[right])) and (left < right):
+                right -= 1
+                print("right is ", s[right])
+            if s[left].lower() != s[right].lower():
                 return False
+            left, right = left+1, right-1
+            print("left and right is ", left, right)
         return True
-
+    
+    def alphaNum(self, c):
+        return (ord('a') <= ord(c) <= ord('z') or
+                ord('A') <= ord(c) <= ord('Z') or
+                ord('0') <= ord(c) <= ord('9'))
