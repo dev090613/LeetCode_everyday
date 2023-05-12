50a92f1fc96a96d65e38cf90bd29cb40d9f4f91c
kokoro <128305501+dev090613@users.noreply.github.com>
2023-05-12 22:19:25 +0900

    Time: 2088 ms (16.85%), Space: 30.9 MB (29.35%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,6 +1,7 @@
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        num_set = set(nums) # O(1)
+        num_set = set(nums) # 이유: To allow O(1) lookups
         # 반환할 값인 가장 긴 길이
         longest = 0
         
@@ -8,10 +9,9 @@ class Solution:
         for num in nums: # O(n)
             # 단편의 시작점을 찾는다.
             if (num - 1) not in num_set:
-                length = 0
+                length = 0 # 1로 두어도 무방
                 while (num + length) in num_set:
                     length += 1
-                longest = max(length, longest)
+                longest = max(length, longest) # scope 주의하기
         return longest
                     
-                
