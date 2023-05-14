c0a6d5d7b9493875fb42646d4e084d7ad0a7fe91
kokoro <128305501+dev090613@users.noreply.github.com>
2023-05-14 22:13:03 +0900

    Time: 102 ms (32.26%), Space: 18 MB (15.06%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,3 +1,5 @@
+# input [0,0,1,1,1,2,2,3,3,4]
+# output [1, 1, 2]
 # Space: O(n)으로 풀기
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
@@ -6,6 +8,4 @@ class Solution:
             if nums[r] != nums[r-1]:
                 nums[l] = nums[r]
                 l += 1
-            r += 1
         return l
-            
