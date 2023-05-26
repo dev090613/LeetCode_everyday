c36252d691f91d258c0564467eeaef4fd9a3b8aa
kokoro <128305501+dev090613@users.noreply.github.com>
2023-05-26 16:58:52 +0900

    Time: 40 ms (68.47%), Space: 23.1 MB (5.38%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,13 +6,17 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         """
+        None head -> head.next -> ... -> None
+        임의의 위치(curr)에서 시작
+        3 pointer: prev, curr, next_node
         """
-        if not head:
-            return None
-        
-        newHead = head
-        if head.next:
-            newHead = self.reverseList(head.next)
-            head.next.next = head
-        head.next = None
-        return newHead
+        def reverse(curr, prev):
+            # Empty list
+            if not curr: 
+                return prev
+            
+            next_node = curr.next
+            curr.next = prev
+            return reverse(next_node, curr)
+                
+        return reverse(head, None)
